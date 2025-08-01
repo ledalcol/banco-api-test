0954fba387ba5310ebab1c2f2798852899354d27
Leticia <le-dalcol@hotmail.com>
2025-08-01 14:25:18 -0300

    Criação de helper de autenticação de criação de token

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,19 +1,15 @@
 const request = require ('supertest');
 const { expect } = require ('chai')
+require('dotenv').config()
+const { obterToken } = require ('../helpers/autenticacao')
 
 describe('Tranferências', () => {
     describe('POST/transferencias', () => {
         it('Deve retornar sucesso com 201 quando o valor for igual ou acima de R$10,00', async () => {
             
-            const respostaLogin = await request (process.env.BASE_URL)
-            .post('/login')
-            .set('Content-Type', 'application/json')
-            .send({
-            'username':'julio.lima',
-            'senha':'123456'
-            })
+            
 
-            const token = respostaLogin.body.token
+            const token = await obterToken ('julio.lima', '123456')
 
             const resposta = await request (process.env.BASE_URL)
             .post('/transferencias')
@@ -32,15 +28,7 @@ describe('Tranferências', () => {
 
          it('Deve retornar falha com 422 quando o valor for abaixo de R$10,00', async () => {
 
-            const respostaLogin = await request (process.env.BASE_URL)
-            .post('/login')
-            .set('Content-Type', 'application/json')
-            .send({
-            'username':'julio.lima',
-            'senha':'123456'
-            })
-
-            const token = respostaLogin.body.token
+            const token = await obterToken ('julio.lima', '123456')
 
             const resposta = await request (process.env.BASE_URL)
             .post('/transferencias')
